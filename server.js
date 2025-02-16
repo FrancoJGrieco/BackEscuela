97213c7179df75dc9756c9be6159aeaa5d85890e
Franco <francojesusgrieco@gmail.com>
2025-02-16 20:38:17 -0300

    Se modifico la configuracion de cors

--- server.js
+++ server.js
@@ -23,8 +23,11 @@ connectToDb()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: true,
-  credentials: true
+  origin: ['http://localhost:3000'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  exposedHeaders: ['Authorization']
 }))
 
 // Rutas cuentas
