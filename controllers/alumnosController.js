070adc6e45c2e2aa47754d65bf532d9696aa24eb
Franco <francojesusgrieco@gmail.com>
2025-02-24 02:49:09 -0300

    Prueba gitignore

--- controllers/alumnosController.js
+++ controllers/alumnosController.js
@@ -4,6 +4,7 @@ const Comision = require('../models/comision')
 
 const fetchAlumnos = async (req, res) => {
   try {
+    console.log('hola')
     const alumnos = await Alumno.find()
       .populate({
         path: 'boletines',
