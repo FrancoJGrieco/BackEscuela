551aaf4a2e11820775784c40824d75542b68e3a8
FrancoJGrieco <francojesusgrieco@gmail.com>
2025-08-04 16:55:04 -0300

    configuracion connectToDb para render

--- config/connectToDb.js
+++ config/connectToDb.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose')
 
 async function connectToDb () {
   try {
-    await mongoose.connect(process.env.DB_URL)
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      ssl: false,
+      directConnection: true
+    })
     console.log('Connected to database')
   } catch (err) {
     console.log(err)
