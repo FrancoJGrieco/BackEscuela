8f0f6c7b81d0e78db0ce3d52c9217284566562bf
FrancoJGrieco <francojesusgrieco@gmail.com>
2025-08-05 10:18:08 -0300

    Se agrego url a cors

--- app.js
+++ app.js
@@ -27,7 +27,9 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: ['http://localhost:3000'],
+  origin: ['http://localhost:3000',
+    'https://frontescuela-production.up.railway.app'
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
